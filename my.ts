2747a89a98323fc367516dfba8987a07889b3744
aniruddha <aniruddha.kudalkar@gmail.com>
2018-03-14 09:08:57 +0530

    first commit

---
+++ my.ts
@@ -0,0 +1,12 @@
+let nm: string = 'android'
+let num = 456
+
+function nmNum(): string {
+    return `android`;
+}
+
+let mg: number | string | boolean = true
+mg = 'android'
+mg = 1235
+
+let an : any = mg
