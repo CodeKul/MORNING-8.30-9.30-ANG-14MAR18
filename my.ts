e320ecbeaa46e31db6bb51eb8b96dae5e0a22cc1
aniruddha <aniruddha.kudalkar@gmail.com>
2018-03-14 09:23:44 +0530

    types : object type variable

--- my.ts
+++ my.ts
@@ -9,4 +9,18 @@ let mg: number | string | boolean = true
 mg = 'android'
 mg = 1235
 
-let an : any = mg
+let an: any = mg
+let fn: (nm: number) => void = function (nm: number) { }
+let fn1: (nm: number) => void = (nm: number) => console.log('hi')
+
+let v1: { [key: string]: number } = {
+    one: 1,
+    two: 2
+}
+
+class Car {
+
+}
+
+let cr: Car = new Car()
+let dt: Date = new Date()
